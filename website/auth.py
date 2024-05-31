0528e64f8012fa62842d16069aa30029b46b2751
Kelvin Mwenda <kelvinmwendadoreen@gmail.com>
2024-05-31 17:35:55 +0300

    final

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_bcrypt import Bcrypt
 from .models import User
-from website import db
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
-bcrypt = Bcrypt()
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,23 +14,23 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         
-        if len(email) < 4:
-            flash('Email must be greater than 4 characters', category='error')
-        elif len(password) < 7:
-            flash('Password must be at least 7 characters', category='error')
-        else:
-            user = User.query.filter_by(email=email).first()
-            if user and bcrypt.check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+        if user := User.query.filter_by(email=email).first():
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully', category='success')
+                login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Login unsuccessful. Please check your email and password', category='error')
+                flash('Incorrect Password, try again', category='error')
+        else:
+            flash('Email does not exist',category='error')
             
-    return render_template("login.html")
+    return render_template('login.html',user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -38,7 +40,6 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        #user = User.query.filter_by(email=email).first()
         if user := User.query.filter_by(email=email).first():
             flash('Email already exists.', category='error')
         elif len(email) < 4:
@@ -50,11 +51,12 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match', category='error')
         else:
-            hashed_password = bcrypt.generate_password_hash(password1).decode('utf-8')
-            new_user = User(email=email, first_name=first_name, password=hashed_password)
+            hashed_password = generate_password_hash(password1,method='pbkdf2:sha256')
+            new_user = User(email = email,first_name=first_name,password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
